c41c97d4cebae76d942f4d0c808aff732743a004
Cryfore (Kirill) <blackhawksk@yandex.ru>
2024-10-14 03:10:02 +0300

    main 0.2

---
+++ String/MyTestWork.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class MyTestWork
+    {
+        public static void PracticeFromVideo()
+        {
+            string[] _categoriesArray = { "Numbers", "Animals" };
+            const string VALUE = "Const";
+
+            Dictionary<int, string> categories = new Dictionary<int, string>()
+            {
+                { 1, _categoriesArray[0] },
+                { 2, _categoriesArray[1] },
+            };
+
+            var categoryQuestions = new Dictionary<int, Dictionary<string, string>>();
+            var questions = new Dictionary<string, string>() { { "PI?", "3.14" } };
+
+            categoryQuestions.Add(1, questions);
+            Console.WriteLine("Select category 1 or 2");
+            var selected = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your category {categories[selected]}");
+            var result = categoryQuestions[selected];
+            foreach (var category in result)
+            {
+                Console.WriteLine(category.Key);
+                int attempts = 0;
+                while (true)
+                {
+                    var answer = Console.ReadLine();
+                    attempts++;
+                    if (attempts >= 3 || answer == category.Value)
+                    {
+                        break;
+                    }
+                }
+                if (attempts < 3)
+                {
+                    Console.WriteLine("you won!");
+                }
+
+                break;
+            }
+        }
+
+        public static void MyTest()
+        {
+            string str = "Hello, Kirill!";
+            str = str.Insert(0, "Love! ");
+            Console.WriteLine(str);
+
+            Console.Write("\n");
+            string[] strArray = str.Split(' ');
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                Console.Write(i + " = " + strArray[i] + "\t");
+            }
+
+            Console.WriteLine("\n");
+            string strTwo = str.Substring(6, 5);
+            Console.WriteLine(strTwo);
+
+            Console.Write("\n");
+            Console.WriteLine("Есть ли запитая? - " + str.Contains(','));
+
+            char chr = 'd';
+            Console.WriteLine(char.IsLetter(chr));
+            Console.WriteLine(char.ToUpper(chr));
+
+            Console.Write("\n");
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"{str} Ohh, no... {strArray[2]} {strArray[0]}\n{strTwo}... {chr} - {char.ToUpper(chr)}");
+            Console.WriteLine(stringBuilder);
+
+            Console.Write("\n");
+            stringBuilder.Append($"\nТут прям все на Английском????");
+            stringBuilder.Append(@"\n");
+
+            Console.WriteLine(stringBuilder.Replace(str, "").ToString());
+        }
+    }
+}
--- String/Program.cs
+++ String/Program.cs
@@ -1,16 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace String
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //1 ConcatenateStrings
+            Console.WriteLine("Обьединение слов в одну строку! Введите вначале одно слово, а после другое: ");
+            Console.WriteLine("Слова обьединились: " + ConcatenateStrings(Console.ReadLine(), Console.ReadLine()));
+
+            //2 GreetUser
+            Console.WriteLine("\nВведите ваше ИМЯ, а после введите ваш ВОЗРАСТ: ");
+            Console.WriteLine(GreetUser(Console.ReadLine(), int.Parse(Console.ReadLine())));
+
+            //3 InfoString
+            Console.WriteLine("\nВведите слово, для отображения кол-ва его символов и визуализации в разных регистрах: ");
+            Console.WriteLine("Информация по введенному слову:\n" + InfoString(Console.ReadLine()));
+
+            //4 ReturnFiveСharacters
+            Console.WriteLine("\nВведите слово или предложение, в котором более 5 символов: ");
+            Console.WriteLine("Возвращенны первые пять символов: " + ReturnFiveСharacters(Console.ReadLine()));
+
+            //5 ConcatenatesElementsArray
+            Console.WriteLine("\nВведите вначале одно предложение, а после другое: ");
+            string[] strArray = { Console.ReadLine(), Console.ReadLine() };
+            Console.WriteLine("Теперь предложение из массива выглядит так: " + ConcatenatesElementsArray(strArray));
+
+            //6 ReplaceWords
+            Console.WriteLine("\nВведите текст для изменения, после слово которое хотите изменить, а далее то слово на которое хотели бы изменить: ");
+            Console.WriteLine("Текст был изменен следующим образом: " + ReplaceWords(Console.ReadLine(), Console.ReadLine(), Console.ReadLine()));
+
+            //MyTestWork.MyTest();
+            //MyTestWork.PracticeFromVideo();
         }
+
+        static string ConcatenateStrings(string oneString, string twoString)
+        {
+            return $"{oneString} {twoString}";
+        }
+
+        static string GreetUser(string username, int age)
+        {
+            return $"Hello, {username}! You are {age} years old.";
+        }
+
+        static string InfoString(string infoString)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Number of characters per line: {infoString.Count()}\n");
+            sb.Append($"Uppercase string: {infoString.ToUpper()}\n");
+            sb.Append($"Lowercase string: {infoString.ToLower()}\n");
+            return sb.ToString();
+        }
+
+        static string ReturnFiveСharacters(string infoString)
+        {
+            return infoString.Substring(0, 5);
+        }
+
+        static StringBuilder ConcatenatesElementsArray(string[] infoString)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < infoString.Length; i++)
+            {
+                stringBuilder.Append(infoString[i]);
+                if (i + 1 != infoString.Length)
+                {
+                    stringBuilder.Append(' ');
+                }
+                else
+                {
+                    stringBuilder.Append('.');
+                }
+            }
+            return stringBuilder;
+        }
+
+        static string ReplaceWords(string text, string word, string replaceWord)
+        {
+            return text.Replace(word, replaceWord);
+        }
+
     }
 }
